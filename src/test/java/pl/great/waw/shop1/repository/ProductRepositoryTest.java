92d5d56e0889baafe3984dcd66bcc06ed61baf10
Maciej Łastówka <maciek240395@gmail.com>
2023-02-23 19:58:02 +0000

    Merged in Shop1_CRUD_Maciek (pull request #1)
    CRUD
    
    * CRUD
    
    
    Approved-by: Paweł Szczepkowski

--- src/test/java/pl/great/waw/shop1/repository/ProductRepositoryTest.java
+++ src/test/java/pl/great/waw/shop1/repository/ProductRepositoryTest.java
@@ -1,5 +1,7 @@
 package pl.great.waw.shop1.repository;
 
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -8,6 +10,7 @@ import pl.great.waw.shop1.domain.Product;
 import java.math.BigDecimal;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNull;
 
 @SpringBootTest
 class ProductRepositoryTest {
@@ -15,20 +18,51 @@ class ProductRepositoryTest {
     private static final String PRODUCT_TITLE = "iPhone 14";
     private static final String DESCRIPTION = "The iPhone is a line of smartphones by Apple";
     private static final BigDecimal PRICE = BigDecimal.valueOf(999);
+    private static final String PRODUCT_TITLE1 = "iPhone";
+    private static final String DESCRIPTION1 = "The iPhone is a line  Apple";
+    private static final BigDecimal PRICE1 = BigDecimal.valueOf(9);
+
+    private Product product;
 
     @Autowired
     private ProductRepository productRepository;
 
+    @BeforeEach
+    void setBefore() {
+        this.product = productRepository.create(new Product(PRODUCT_TITLE, DESCRIPTION, PRICE));
+    }
+
+    @AfterEach
+    void tearDown() {
+        this.productRepository.deleteAll();
+    }
+
     @Test
     void create() {
-        Product product = this.productRepository.create(new Product(PRODUCT_TITLE, DESCRIPTION, PRICE));
         Product savedProduct = this.productRepository.findById(product.getId());
 
-        assertEquals(product,savedProduct );
+        assertEquals(product, savedProduct);
 
     }
 
     @Test
     void findById() {
+        Product byId = productRepository.findById(product.getId());
+        assertEquals(product, byId);
+    }
+
+    @Test
+    void deleteById() {
+        productRepository.deleteById(product.getId());
+        assertNull(productRepository.findById(product.getId()));
+    }
+
+    @Test
+    void update() {
+        Product product1 = new Product(PRODUCT_TITLE1, DESCRIPTION1, PRICE1);
+        Product updated = this.productRepository.update(product1);
+
+        assertEquals(updated, product1);
+
     }
 }
