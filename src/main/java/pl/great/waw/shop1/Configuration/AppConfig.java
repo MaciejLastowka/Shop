cd6d4a928da4f65847826bca5fcbdf96bc69f8dc
Maciej Łastówka <maciek240395@gmail.com>
2023-03-16 14:02:29 +0000

    Merged in Shop-6_Maciek_liquibase (pull request #9)
    liquibase
    
    * liquibase
    
    
    Approved-by: Paweł Szczepkowski

--- src/main/java/pl/great/waw/shop1/Configuration/AppConfig.java
+++
@@ -1,9 +0,0 @@
-package pl.great.waw.shop1.Configuration;
-
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.Configuration;
-
-@Configuration
-@ComponentScan(basePackages = "pl.great.waw.shop1.Mapper")
-public class AppConfig {
-}
